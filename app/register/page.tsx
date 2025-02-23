7eafe63ef07d39c5598925c44c5ca8d86eb62240
Satendra Kaushik <satendrakaushik2002@gmail.com>
2025-02-24 03:41:56 +0530

    update

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -8,15 +8,14 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import Link from "next/link"
-
+// data aaasdsadsdasdasdsadasddada
 export default function Register() {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
-    // Handle registration logic here
+    
     console.log("Register with:", name, email, password)
   }
 
